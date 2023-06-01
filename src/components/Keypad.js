30a0ef7bd64cbfb7904fc2aac0b79c250466626a
Aruna Turay <tura_arun@yahoo.com>
2023-06-01 16:33:15 -0400

    final push

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+function handleChange () {
+    console.log("Entering password...")
+    //console.log(event.target.value)
+}
+
     return (
-        <div></div>
-    )
+      
+        <input type="password" onChange={handleChange} placeholder="Entering password..."/>
+      
+    );
 }
 
 export default Keypad;
